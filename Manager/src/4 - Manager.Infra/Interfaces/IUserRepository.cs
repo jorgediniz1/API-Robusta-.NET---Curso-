e5a6d0494300771a86289b1f1ef4ef486f2f934d
Jorge Diniz <onediniz2@gmail.com>
2022-09-26 22:46:29 -0300

    Criação dos repositorios

---
+++ Manager/src/4 - Manager.Infra/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+namespace Manager.Infra.Interfaces
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<User> GetByEmail(string email);
+        Task<List<User>> SearchByEmail(string email);
+        Task<List<User>> SearchByName(string name);
+    }
+}
